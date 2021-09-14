2537de4e0e3f9418ce505e723c707a284b778901
gaytomycode <hi@gaytomycode.com>
2021-09-14 19:44:20 +0200

    feature(celery): added test celery task

--- dedo_lms/payment/tasks.py
+++ dedo_lms/payment/tasks.py
@@ -6,11 +6,18 @@ from dedo_lms.celery import app
 
 
 @app.task
-def test_task():
+def test_task(email_list):
+    from urllib.request import urlopen
+    import json
+    resp = json.loads(urlopen('http://ron-swanson-quotes.herokuapp.com/v2/quotes').read())[0]
+    message = '''<p>Morning,</p>
+    <p>How was sleep fighting? Did you know that ''' + resp + '''.</p>
+<p>Sincerely,</p>
+<p>Ron Swanson</p>'''
     send_mail(
-        subject='test_task',
+        subject='Ron Swanson',
         from_email=settings.FROM_EMAIL,
-        recipient_list=['hi@gaytomycode.com'],
-        message='test',
-        html_message='test',
+        recipient_list=email_list,
+        message=message,
+        html_message=message,
     )
