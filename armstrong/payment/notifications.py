49d9ffdd36e82a2139c2e076ba37b958e200a1a0
gaytomycode <hi@gaytomycode.com>
2021-09-29 03:59:26 +0200

    bugfix(payment.notification): added notification for invoice

--- armstrong/payment/notifications.py
+++ armstrong/payment/notifications.py
@@ -4,12 +4,14 @@ from django.conf import settings
 from django.template.loader import render_to_string
 
 
-def invoice_email(user):
+def invoice_email(invoice):
     send_mail(
         subject='Armstrong Invoice',
         from_email=settings.FROM_EMAIL,
-        recipient_list=user.unconfirmed_emails,
-        message=render_to_string('emails/confirmation_code.html', {'user': user, 'settings': settings}),
-        html_message=render_to_string('emails/confirmation_code.html', {'user': user, 'settings': settings}),
+        recipient_list=[invoice.user.email],
+        # message=render_to_string('emails/confirmation_code.html', {'user': user, 'settings': settings}),
+        message='INVOCIE',
+        # html_message=render_to_string('emails/confirmation_code.html', {'user': user, 'settings': settings}),
+        html_message='INVOCIE',
         fail_silently=not settings.DEBUG,
     )
