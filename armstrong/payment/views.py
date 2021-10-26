5b8f2e05086a240c2c1fb465f7ba6a5ba7d132c8
gaytomycode <hi@gaytomycode.com>
2021-10-26 22:02:14 +0200

    feature(fixed typo in checkout)

--- armstrong/payment/views.py
+++ armstrong/payment/views.py
@@ -155,7 +155,7 @@ def subscribe_done(request):
         if len(items) > 1:
             for item in items:
                 if item['name'] == 'PROMOCODE':
-                    promcode_amount = item['amount_cents']
+                    promcode_price = item['amount_cents']
                 else:
                     item_name = item['name']
                     item_price = item['amount_cents']
